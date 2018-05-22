0ac74d85dd22ce5930b88eacb2ee5791fdbe2bab
LCEST <33326932+LCEST@users.noreply.github.com>
2018-05-22 15:27:39 -0600

    Proyecto casi terminado

--- bdPelis.py
+++ bdPelis.py
@@ -1,101 +1,163 @@
-"""
-Gustavo De Leon  17085
-Luis Esturban  17256
-Carlos Chew 17507
-"""
-
+#Luis Esturban
 from neo4jrestclient.client import GraphDatabase
-from neo4jrestclient import client
-
-#Creacion de base de datos de grafos con su nombre y contraseña
-db = GraphDatabase("http://localhost:7474", username="TuPeli", password="pelicula")
-
-#cracion de nodos principales(En proceso)
+ 
+db = GraphDatabase("http://localhost:7474", username="neo4j", password="123")
+ 
+# Create some nodes with labels
 generos = db.labels.create("Generos")
 peliculas = db.labels.create("Peliculas")
-
-# Generos de las Peliculas(En proceso)
-terror = db.nodes.create(name="Terror")
-romance = db.nodes.create(name="Romance")
-accion = db.nodes.create(name="Accion")
-comedia = db.nodes.create(name="Comedia")
-drama = db.nodes.create(name="Drama")
-ficcion = db.nodes.create(name="Ficcion")
-musical = db.nodes.create(name="Musical")
-suspenso = db.nodes.create(name="Suspenso")
-
-# agregar generos (En proceso)
-generos.add(terror, romance, accion, comedia, comedia, drama, ficcion, musical, suspenso)
+actores = db.labels.create("Actores")
 
 
-# Creando cada pelicula (En proceso)
-capitanA1 = db.nodes.create(name="Capitan America 1")
-jurassic1 = db.nodes.create(name="Jurassic Park 1")
-chucky1 = db.nodes.create(name= "Chucky 1")
-viernes13 = db.nodes.create(name="Viernes 13")
-purga = db.nodes.create(name="La Purga")
-infinityW = db.nodes.create(name="Infinity War")
-scaryM = db.nodes.create(name="Scary Movie")
-high = db.nodes.create(name="High School Musical")
-lalala = db.nodes.create(name="Lalala")
-starW = db.nodes.create(name="Star Wars")
-indianaJ = db.nodes.create(name="Indiana Jones")
-titanic = db.nodes.create(name="Titanic")
-harryP = db.nodes.create(name="Harry Potter")
+def menu():
+    print ("***********************************************")
+    print ("Sistema de recomendacion medica")
+    print ("1. Ingresar Genero")
+    print ("2. Ingresar Pelicula")
+    print ("3. Ingresar Actor")
+    print ("4. Relacion entre Genero y Pelicula")
+    print ("5. Relacion entre Pelicula y Actor")
+    print ("6. Consultar Generos")
+    print ("7. Consultar Actores")
+    print ("8. Consultar Peliculas por Genero")
+    print ("9. Consultar Peliculas por Actor")
+    print ("10. Salir")
+    print ("***********************************************")
+    print (" ")
+#Este bloque es para prueba
+#Hacer lo mismo solo que automatico
+#----------------------------------------------------------------    
+#Funcion para ingresar genero
+def ingresarGen():
+    gen = input("Ingrese el nombre del Genero: ")
+    genero = db.nodes.create(nombreGen=gen)
+    generos.add(genero)
+#Funcion para agregar pelicula
+def ingresarPel():
+    pel = input("Ingrese el nombre del Genero: ")
+    pelicula = db.nodes.create(nombrePel=pel)
+    peliculas.add(pelicula)
+#Funcion para agregar Actor
+def ingresarAct():
+    act = input("Ingrese el nombre del Genero: ")
+    actor = db.nodes.create(nombreAct=act)
+    actores.add(actor)
+#Gustavo este es el que hay que cambiar para que sea alreves
+#Funcion para relacionar peliculas con generos
+def relacionPel_Gen():
+     gen1 = input("Ingrese el genero: ")
+     pel1 = input("Ingrese la pelicula: ")
+     try: 
+         (generos.get(nombreGen=gen)[0]).relationships.create("pertenece", peliculas.get(nombrePel=pell)[0])
+        
+     except:
+         print("No existe algun paciente de los dos o esta mal escrito")
+         print(" ")
+         print(" ")
+#Funcion para relacionar actores con pelicula
+def relacionAct_Pel():
+     act1 = input("Ingrese el actor: ")
+     pel2 = input("Ingrese la pelicula: ")
+     try: 
+         (actores.get(nombreAct=act1)[0]).relationships.create("actua", peliculas.get(nombrePel=pel2)[0]) 
+     except:
+         print("No existe algun paciente de los dos o esta mal escrito")
+         print(" ")
+         print(" ")
+#----------------------------------------------------------------------------------------------------------------------------------------
 
-#agregando las peliculas
-peliculas.add(capitanA1, jurassic1, chucky1, viernes13, purga, infinityW, scaryM, high, lalala, starW, indianaJ, titanic, harryP)
+#Funcion para consultar genero
+def consultarGen():
+    try:
+        query = "MATCH (n:Generos) RETURN n"
+        results = db.query(query, data_contents=True)
+        a = results.rows
+        for list in a:
+            for x in list:
+                print (x)
+    except:
+         print("No hay ningun doctor con esa especialidad o esta mal escroto")
+         print(" ")
+         print(" ")
+#Funcion para consultar actor
+def consultarAct():
+    conAct = input("Ingrese la especialidad: ")
+    try:
+        query = "MATCH (n:Actores {nombreAct:'"+conAct+"'}) RETURN n"
+        results = db.query(query, data_contents=True)
+        a = results.rows
+        for list in a:
+            for x in list:
+                print (x)
+    except:
+         print("No hay ningun doctor con esa especialidad o esta mal escroto")
+         print(" ")
+         print(" ")
+#Funcion para consultar pelicula por medio de su genero
+def consultarPel_Gen():
+    gen3 = input("Ingrese el genero que desea buscar: ")
+    try:
+         query = "MATCH (p:Peliculas{nombrePel:'"+gen3+"'})-[:pertenece]->(d:Generos) return d"
+         results = db.query(query, data_contents=True)
+         a = results.rows
+         b = []
+         for x in a:
+             if x not in b:
+                 b.append(x)
+                 print (x)
+         print(" ")
+         print(" ")
+    except:
+         print("No tiene algun amigo que le recomiende esta especialidad ")
+         print("O lo escribio mal ")
+         print(" ")
+#Funcion para consultar pelicula por medio de su actor    
+def consultarPel_Act():
+    gen3 = input("Ingrese el genero que desea buscar: ")
+    try:
+         query = "MATCH (p:Actores{nombreAct:'"+gen3+"'})-[:actua*1..2]->(d:Peliculas{nombrePel:'"+pel3+"'}) return d"
+         results = db.query(query, data_contents=True)
+         a = results.rows
+         b = []
+         for x in a:
+             if x not in b:
+                 b.append(x)
+                 print (x)
+         print(" ")
+         print(" ")
+    except:
+         print("No tiene algun amigo que le recomiende esta especialidad ")
+         print("O lo escribio mal ")
+         print(" ")
 
+#Ciclo principal para menu  
+opcion = 0
+while (opcion != 8):
+	# Mostramos el menu
+	menu()
+ 
+	# solicitamos una opción al usuario
+	opcion = int(input("Ingrese el numero que desea realizar: "))
+ 
+	if opcion==1:		
+		ingresarGen()
+	elif opcion==2:
+		ingresarPel()
+	elif opcion==3:
+		ingresarAct()
+	elif opcion==4:
+		relacionPel_Gen()
+	elif opcion==5:
+		relacionAct_Pel()
+	elif opcion==6:
+		consultarGen()
+	elif opcion==7:
+		consultarAct()
+	elif opcion==8:
+		consultarPel_Gen()
+	elif opcion==9:
+		consultarPel_Act()
+	elif opcion==10:
+		sys.exit()
 
-#Conectando cada genero con pelicula (En proceso)
-capitanA1.relationships.create("es de ",accion)
-capitanA1.relationships.create("es de ",comedia)
-capitanA1.relationships.create("es de ",ficcion)
-
-jurassic1.relationships.create("es de ",accion)
-jurassic1.relationships.create("es de ",ficcion)
-jurassic1.relationships.create("es de ",suspenso)
-
-chucky1.relationships.create("es de ",terror)
-chucky1.relationships.create("es de ",suspenso)
-chucky1.relationships.create("es de ",comedia)
-
-viernes13.relationships.create("es de ",terror)
-viernes13.relationships.create("es de ",accion)
-viernes13.relationships.create("es de ",suspenso)
-
-purga.relationships.create("es de ",suspenso)
-purga.relationships.create("es de ",terror)
-purga.relationships.create("es de ",drama)
-
-infinityW.relationships.create("es de ",comedia)
-infinityW.relationships.create("es de ",suspenso)
-infinityW.relationships.create("es de ",accion)
-
-scaryM.relationships.create("es de ",comedia)
-scaryM.relationships.create("es de ",accion)
-scaryM.relationships.create("es de ",suspenso)
-
-high.relationships.create("es de ",musical)
-high.relationships.create("es de ",romance)
-high.relationships.create("es de ",comedia)
-
-lalala.relationships.create("es de ",comedia)
-lalala.relationships.create("es de ",romance)
-lalala.relationships.create("es de ",drama)
-
-starW.relationships.create("es de ",ficcion)
-starW.relationships.create("es de ",accion)
-
-indianaJ.relationships.create("es de ",accion)
-indianaJ.relationships.create("es de ",ficcion)
-
-
-titanic.relationships.create("es de ",drama)
-titanic.relationships.create("es de ",romance)
-titanic.relationships.create("es de ",suspenso)
-
-harryP.relationships.create("es de ",ficcion)
-harryP.relationships.create("es de ",accion)
-
-
+                
